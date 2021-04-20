ddc582ea64b13360cba0f9eb5626fc74f90d4f22
Maia Naidich <manaidich@gmail.com>
2021-04-20 14:22:14 -0300

    Primer commit de prueba

---
+++ Clase 20-04-2021/Ejercicio10_Practica_Manipulacion_de_archivos.py
@@ -0,0 +1,4 @@
+#Ejercicio 10
+#Escribí un programa que añada a un archivo dado todos los archivos de texto (.txt) que hayan en una determinada carpeta.
+
+carpeta="F:\\Fundamentos_de_Informática\\Textos"
