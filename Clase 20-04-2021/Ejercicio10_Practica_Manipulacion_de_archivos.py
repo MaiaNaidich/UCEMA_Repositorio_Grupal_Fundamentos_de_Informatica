ec89f5ac67c61d1b5b9fa28bb210cb10fd1a338d
Maia Naidich <manaidich@gmail.com>
2021-04-20 14:42:11 -0300

    Ejercicio Terminado (creo, falta chequearlo en la temrinal de Python)

--- Clase 20-04-2021/Ejercicio10_Practica_Manipulacion_de_archivos.py
+++ Clase 20-04-2021/Ejercicio10_Practica_Manipulacion_de_archivos.py
@@ -2,3 +2,13 @@
 #Escribí un programa que añada a un archivo dado todos los archivos de texto (.txt) que hayan en una determinada carpeta.
 
 carpeta="F:\\Fundamentos_de_Informática\\Textos"
+texto_destino="F:\\Fundamentos_de_Informática\\Textos\\Texto_Destino"
+texto_a_combinar=""
+with open(carpeta,"r") as carpeta:
+    for texto in carpeta:
+        texto_leido=texto.read()
+        texto_a_combinar+=texto_leido
+        texto_a_combinar+="\n"
+    print(texto_a_combinar)
+with open(texto_destino,"w") as texto_destino:
+    texto_destino.write(texto_a_combinar)
