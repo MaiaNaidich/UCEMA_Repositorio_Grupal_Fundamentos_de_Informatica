93030154f511b388980bd222b27a8dfb16938827
MaximoCabezasFernandez <machu.cabezas@outlook.com>
2021-04-22 09:08:37 -0300

    Create Ejercicio 9 PRUEBA.py

---
+++ Clase 20-04-2021/Ejercicio 9 PRUEBA.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Ejercicio 9 PRUEBA
+
+# Escribí un programa que extraiga los caracteres que estén entre guiones en un string. (String de ejemplo: "Hoy estuvimos trabajando con re -regular expression- en python -con VSCode-")
+
+# In[ ]:
+
+
+
+
