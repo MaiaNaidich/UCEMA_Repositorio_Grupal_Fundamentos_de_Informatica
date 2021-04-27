96870efc30553498ddf44f0eeda160a9f9a5d7b1
mateosceia <82593637+mateosceia@users.noreply.github.com>
2021-04-27 14:22:24 -0300

    Update Ejercicios_2.py

--- Clase 27-04-2021/Ejercicios_2.py
+++ Clase 27-04-2021/Ejercicios_2.py
@@ -3,12 +3,3 @@ def volar(self, kms):
     if self.energia-(10 + kms)>=0:
         self.energia -= 10 + kms
 
-#Ejercicio 3
-class Notebook:
-    def __init__(self, marca, modelo, precio):
-        self.marca=marca
-        self.modelo=modelo
-        self.precio=precio
-
-    def desceunto(self, descuento):
-        self.precio-=(descuento/100)*precio
