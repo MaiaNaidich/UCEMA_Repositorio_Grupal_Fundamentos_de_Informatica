0f6b40c862844f7f3be4aa04b82e22bd1da6cd9f
mateosceia <mateosceia.ms@gmail.com>
2021-04-27 14:56:42 -0300

    Create Ejercicio_4.py

---
+++ Clase 27-04-2021/Ejercicio_4.py
@@ -0,0 +1,18 @@
+class contador:
+    def __init__(self, valor_inicial):
+        self.valor=valor_inicial
+    
+    def inc(self):
+        self.valor+=1
+    
+    def dis(self):
+        self.valor-=1
+    
+    def reset(self):
+        self.valor=0
+    
+    def valorActual(self):
+        self.valor
+    
+    def valorNuevo(self, valor_nuevo):
+        self.valor=valor_nuevo
